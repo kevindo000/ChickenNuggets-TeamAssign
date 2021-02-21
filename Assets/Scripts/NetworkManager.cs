96907ca1ecaf1853adb1f3ef9b7810b6b0814e71
Gal Egozi <geegozi@gmail.com>
2021-02-20 20:30:15 -0600

    Adding the forward and backwards buttons. Including some test assets from Unity and some custom assets

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -6,47 +6,93 @@ using UnityEngine.Networking;
 
 public class NetworkManager : MonoBehaviour
 {
+    private UnityEngine.Object[] assets;
+    private GameObject[] instAssets;
+    private int position = 0;
+    public string url = String.Empty;
     IEnumerator getRequest(string url, Action<UnityWebRequest> callback)
     {
-        using(UnityWebRequest request = UnityWebRequest.Get(url))
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
         {
             yield return request.SendWebRequest();
             callback(request);
         }
     }
+
+    public void nextButtonPushed()
+    {
+        try
+        {
+            int current = position;
+            position = (position + 1) % instAssets.Length;
+            string temp = String.Format("Changing position from {0} to {1}", current, position);
+            Debug.Log(temp);
+            instAssets[current].SetActive(false);
+            instAssets[position].SetActive(true);
+        }
+        catch(Exception e)
+        {
+            Debug.LogError("Error in nextButtonPushed");
+            Debug.LogError(e);
+        }
+    }
+
+    public void backButtonPushed()
+    {
+        try
+        {
+            int current = position;
+            position = (position + instAssets.Length - 1) % instAssets.Length;
+            string temp = String.Format("Changing position from {0} to {1}", current, position);
+            Debug.Log(temp);
+            instAssets[current].SetActive(false);
+            instAssets[position].SetActive(true);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error in backButtonPushed");
+            Debug.LogError(e);
+        }
+    }
+
     // Start is called before the first frame update
     IEnumerator Start()
     {
-        Debug.Log("Hello!");
-        string url = "https://arvrclassstorage.blob.core.windows.net/models/PennyTestBundle/penny_prefab";
-        //string url = "file:///" + Application.dataPath + "/AssetBundles/" + "penny_prefab";
+        int i = 0;
+        //string url = "https://arvrclassstorage.blob.core.windows.net/models/PennyTestBundle/penny_prefab";
         UnityEngine.Networking.UnityWebRequest request
             = UnityWebRequestAssetBundle.GetAssetBundle(url, 0);
         yield return request.SendWebRequest();
         AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(request);
-        UnityEngine.Object[] objects = bundle.LoadAllAssets();
-        foreach(UnityEngine.Object obj in objects)
+        assets = bundle.LoadAllAssets();
+        int assetCount = 0;
+        for(i = 0; i < assets.Length; i++)
         {
-            GameObject g = (GameObject)obj;
-            GameObject used = Instantiate(g);
-            Vector3 scale = used.transform.localScale;
-            scale *= 100;
-            used.transform.localScale = scale;
-            used.SetActive(true);
+            if(assets[i].GetType() == typeof(GameObject))
+            {
+                assetCount++;
+            }
         }
-        //GameObject cube = bundle.LoadAsset<GameObject>("Cube");
-        //GameObject sprite = bundle.LoadAsset<GameObject>("Sprite");
-        //foreach (string ob in bundle.GetAllAssetNames())
-        //{
-        //    Debug.Log(ob);
-        //}
-        //Instantiate(cube);
-        //Instantiate(sprite);
+        Debug.Log(assetCount);
+        instAssets = new GameObject[assetCount];
+        i = 0;
+        foreach(UnityEngine.Object obj in assets)
+        {
+            if(obj.GetType() == typeof(GameObject))
+            {
+                GameObject g = (GameObject)obj;
+                GameObject used = Instantiate(g);
+                used.SetActive(false);
+                instAssets[i] = used;
+                i++;
+            }
+        }
+        instAssets[0].SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
